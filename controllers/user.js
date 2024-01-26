cbf33abf95eba6e06ef62ce8d65a24d5fd3cf5c9
Alexandr Popov <alexep130@yandex.ru>
2024-01-26 15:39:36 +0300

    final 26.01

--- controllers/user.js
+++ controllers/user.js
@@ -118,7 +118,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key');
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.status(200).send({ token });
     })
     .catch(next);
